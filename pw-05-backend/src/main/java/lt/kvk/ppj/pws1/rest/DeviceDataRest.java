4b426f106c6ec576479da25cfeacdd28c9056170
vilius <viliusj.jankauskas@gmail.com>
2019-01-18 09:20:24 +0200

    Device data edit, implementatin of proximity

--- pw-05-backend/src/main/java/lt/kvk/ppj/pws1/rest/DeviceDataRest.java
+++ pw-05-backend/src/main/java/lt/kvk/ppj/pws1/rest/DeviceDataRest.java
@@ -1,5 +1,6 @@
 package lt.kvk.ppj.pws1.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -39,9 +40,45 @@ public class DeviceDataRest implements DeviceDataApi {
 
 	@Override
 	public ResponseEntity<Void> addDeviceData(@Valid DeviceData deviceData) {
+		
+		// Mock list of transmitters
+		//_______________________________________
+		List<Object> transmitter1 = new ArrayList<>();
+		transmitter1.add(deviceData.getObjectId1());
+		transmitter1.add(deviceData.getSignal1());
+		transmitter1.add(deviceData.getTransmitterId1());
+		
+		List<Object> transmitter2 = new ArrayList<>();
+		transmitter2.add(deviceData.getObjectId2());
+		transmitter2.add(deviceData.getSignal2());
+		transmitter2.add(deviceData.getTransmitterId2());
+		
+		List<Object> transmitter3 = new ArrayList<>();
+		transmitter3.add(deviceData.getObjectId3());
+		transmitter3.add(deviceData.getSignal3());
+		transmitter3.add(deviceData.getTransmitterId3());
+		
+		List<List> transmitterList = new ArrayList<>();
+		transmitterList.add(transmitter1);
+		transmitterList.add(transmitter2);
+		transmitterList.add(transmitter3);
+		//_______________________________________
+        //
+		
+		System.out.println("Device ALL info:    " + deviceData);
+		List closestTransmitter = proximity(transmitterList);
+		
+		//List closestTransmitter = proximity(transmitterList);
+		System.out.println("Device id:    " + closestTransmitter.get(2));
+	
 
-		final ObjectEntity object = objectRepository.findOneByObjectId(deviceData.getObjectId1());
-		final BeaconEntity beacon = beaconRepository.findOneByBeaconId(deviceData.getTransmitterId1());
+		final ObjectEntity object = objectRepository.findOneByObjectId(String.valueOf(closestTransmitter.get(0)));
+		final BeaconEntity beacon = beaconRepository.findOneByBeaconId( String.valueOf(closestTransmitter.get(2)));
+		System.out.println("Beacon:    " + beacon);
+		
+//		final ObjectEntity object = objectRepository.findOneByObjectId("ss55");
+//		//final BeaconEntity beacon = beaconRepository.findOneByBeaconId("Ak50");
+//		
 		final List<BeaconInPlanEntity> beaconInPlanAllData = beaconInPlanRepository
 				.findBeaconInPlanByBeaconId(beacon.getId());
 
@@ -56,6 +93,7 @@ public class DeviceDataRest implements DeviceDataApi {
 
 		}
 
+		System.out.println("Device d ALL info:    " + deviceData);
 		// Included Plan object info and Beacon object info
 		System.out.println("BeaconInPlan ALL info:    " + beaconInPlanEntity);
 		// ALL Beacon object info
@@ -66,6 +104,25 @@ public class DeviceDataRest implements DeviceDataApi {
 		System.out.println("Object info:    " + object.getObjName() + ".get...");
 
 		return null;
+	}
+	
+	
+	public List proximity(List<List>transmitterList) {
+		List closestTransmitter = null;
+		float strongestSignal = (float) transmitterList.get(0).get(1);
+		for (List transmitter : transmitterList) {
+			if((float) transmitter.get(1) >= strongestSignal) {
+				closestTransmitter = transmitter;
+				strongestSignal = (float) transmitter.get(1);
+			}
+		}
+		
+		
+		System.out.println("proximity: "+closestTransmitter);
+		return closestTransmitter;
+		
+		
+		
 	}
 
 }
