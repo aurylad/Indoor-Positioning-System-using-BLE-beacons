90ecbaff92d6c779683d2ee5ee549d1016b5529e
Dovydas Auryla <auryla.d@gmail.com>
2019-01-23 09:09:10 +0200

    Frontend swagger impl, menu view, plan ft upload

--- ips-forntend/src/app/components/plan-add/plan-add.component.ts
+++ ips-forntend/src/app/components/plan-add/plan-add.component.ts
@@ -9,7 +9,68 @@ export class PlanAddComponent implements OnInit {
 
   constructor() { }
 
+  indexForCoordToShow = 0;
+
+  // Variables for distance calculation between two selected points
+  cXFirst;
+  cYFirst;
+  cXSecond;
+  cYSecond;
+
   ngOnInit() {
+    (<HTMLInputElement>document.getElementById("measureCheckBox")).disabled = true;
   }
 
+  processForm() {
+
+  }
+
+  measuringToolCheckBox() {
+    let element = <HTMLInputElement>document.getElementById("myonoffswitch");
+    if (element.checked) {
+      (<HTMLInputElement>document.getElementById("measureCheckBox")).disabled = false;
+      this.showCoords(event);
+    } else {
+      (<HTMLInputElement>document.getElementById("measureCheckBox")).disabled = true;
+    }
+  }
+
+  url: any = " ";
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = (<FileReader>event.target).result;
+      }
+    }
+
+  }
+
+  showCoords(event) {
+    this.indexForCoordToShow++;
+
+    if (this.indexForCoordToShow == 2) {
+      var cX = event.clientX;
+      var cY = event.clientY;
+      this.cXFirst = cX;
+      this.cYFirst = cY;
+    } else if (this.indexForCoordToShow > 2) {
+      cX = event.clientX;
+      cY = event.clientY;
+      this.cXSecond = cX;
+      this.cYSecond = cY;
+
+      var resultBetweenX = this.cXFirst - this.cXSecond;
+      var resultBetweenY = this.cYFirst - this.cYSecond;
+
+      if (Math.abs(resultBetweenX) > Math.abs(resultBetweenY)) {
+        document.getElementById("demo").innerHTML = "Pasirinktas atstumas-   " + Math.abs(resultBetweenX) + " pixels";
+      } else {
+        document.getElementById("demo").innerHTML = "Pasirinktas atstumas-   " + Math.abs(resultBetweenY) + " pixels";
+      }
+
+      this.indexForCoordToShow = 1;
+    }
+  }
 }
--- pw05-frontend/src/app/app-routing.model.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTE_PRICE_ADD = 'price-add';
-export const ROUTE_PRICES = 'prices';
-
-export const ROUTE_GUEST_VISIT_ADD = 'guest-visit-add';
-export const ROUTE_GUEST_VISITS = 'guest-visits';
-
-export const ROUTE_REPORT = 'report';
