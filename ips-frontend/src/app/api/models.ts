e3254289fca85775ebe2fe7b6058541f6c67ceba
Dovydas Auryla <auryla.d@gmail.com>
2019-06-17 11:07:39 +0300

    'Final'

--- ips-frontend/src/app/api/models.ts
+++
@@ -1,6 +0,0 @@
-export { DeviceData } from './models/device-data';
-export { Plan } from './models/plan';
-export { Log } from './models/log';
-export { Beacon } from './models/beacon';
-export { BeaconInPlan } from './models/beacon-in-plan';
-export { TrackedObject } from './models/tracked-object';
--- ips-frontend/src/app/api/models/beacon.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-export interface Beacon {
-  id?: number;
-
-  /**
-   * Beacon identification code
-   */
-  beaconId?: string;
-}
